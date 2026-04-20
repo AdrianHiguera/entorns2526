496fbe7d411570d46f880ab0a8a97b8a04b778c2
AdrianHiguera <ahiguera.edt@gmail.com>
2026-04-20 15:00:30 +0200

    a

--- Prototip3/server/DaoServer.py
+++ Prototip3/server/DaoServer.py
@@ -1,12 +1,17 @@
 from dataclasses import dataclass, asdict
+import hashlib
 from flask import jsonify
 import mysql.connector
+import uuid
+from time import time
+import random
 
 class UserDAO:
     
     def connectBBDD(self):
         connection = mysql.connector.connect(
             host="localhost",
+            port=3306,
             user="root",
             password="root",
             database="tapatapp"
@@ -19,7 +24,7 @@ class UserDAO:
         cursor = con.cursor(dictionary=True)
         query="""
             SELECT * FROM User
-            WHERE (username = %s OR email = %s) AND PASSWORD = %s
+            WHERE (username = %s OR email = %s) AND password = %s
         """
 
         cursor.execute(query, (identifier, identifier, password))
@@ -27,7 +32,49 @@ class UserDAO:
         cursor.close()
         con.close()
         return user
+    
+    def setTokenUser(self, username):
+        # connectar BBDD
+        con = self.connectBBDD()
+        cursor = con.cursor(dictionary=True)
+
+        # generar token
+        token=self.getHash()
+
+        # update a BBDD cap token al usuari per username
+        query = "UPDATE User SET token ='" + token + "'WHERE username = %s" + username
+        print(query)
+    
+        cursor.execute(query)
+        con.commit()
+
+        # close BBDD
+        cursor.close()
+        con.close()
+
+    def getHash2(self,username):
+        milliseconds = str(time()*1000)
+        data = username + milliseconds
+        hash_object = hashlib.sha256(data.encode('utf-8'))
+        return hash_object.hexdigest()
+    
+    def getHash(self):
+        milliseconds = str(time()*random.randrange(10000))
+        data = milliseconds
+        hash_object = hashlib.sha256(data.encode('utf-8'))
+        return hash_object.hexdigest()
 
 dao=UserDAO()
-u=dao.login("mare","mare")
+print(dao.getHash("User1"))
+
+u=dao.login("masdasfare", "pare")
 print(u)
+
+milliseconds = str(time()*1000)
+print("Time in milliseconds since epoch", milliseconds)
+data = "Hola Mundo" + milliseconds
+print(data)
+
+hash_object = hashlib.sha256(data.encode('utf-8'))
+token = hash_object.hexdigest()
+print(token)

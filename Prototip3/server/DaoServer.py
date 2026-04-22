531efc6fe42178078236327c5414b47615116d2f
AdrianHiguera <ahiguera.edt@gmail.com>
2026-04-22 13:53:33 +0200

    a

--- Prototip3/server/DaoServer.py
+++ Prototip3/server/DaoServer.py
@@ -1,80 +1,133 @@
 from dataclasses import dataclass, asdict
 import hashlib
-from flask import jsonify
+from flask import Flask, jsonify, request
 import mysql.connector
-import uuid
 from time import time
 import random
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# =======================
+# CONFIG FLASK
+# =======================
+
+app = Flask(__name__)
+
+# =======================
+# DAO
+# =======================
 
 class UserDAO:
     
     def connectBBDD(self):
-        connection = mysql.connector.connect(
+        return mysql.connector.connect(
             host="localhost",
             port=3306,
             user="root",
             password="root",
             database="tapatapp"
         )
-        return connection
 
     def login(self, identifier, password):
-        # connexio a BBDD
-        con=self.connectBBDD()
-        cursor = con.cursor(dictionary=True)
-        query="""
-            SELECT * FROM User
-            WHERE (username = %s OR email = %s) AND password = %s
-        """
-
-        cursor.execute(query, (identifier, identifier, password))
-        user = cursor.fetchone()
-        cursor.close()
-        con.close()
-        return user
-    
-    def setTokenUser(self, username):
-        # connectar BBDD
         con = self.connectBBDD()
         cursor = con.cursor(dictionary=True)
 
-        # generar token
-        token=self.getHash()
+        query = """
+            SELECT * FROM User
+            WHERE (username = %s OR email = %s)
+        """
 
-        # update a BBDD cap token al usuari per username
-        query = "UPDATE User SET token ='" + token + "'WHERE username = %s" + username
-        print(query)
-    
-        cursor.execute(query)
-        con.commit()
+        cursor.execute(query, (identifier, identifier))
+        user = cursor.fetchone()
 
-        # close BBDD
+        if user and check_password_hash(user['password'], password):
+            print("LOGIN OK")
+            token = self.setTokenUser(user['username'])
+            user['token'] = token
+        else:
+            print("LOGIN FAIL")
+            user = None
+
         cursor.close()
         con.close()
 
-    def getHash2(self,username):
-        milliseconds = str(time()*1000)
+        return user
+
+    def setTokenUser(self, username):
+        con = self.connectBBDD()
+        cursor = con.cursor()
+
+        token = self.getHash(username)
+
+        query = "UPDATE User SET token = %s WHERE username = %s"
+        
+        cursor.execute(query, (token, username))
+        con.commit()
+
+        cursor.close()
+        con.close()
+
+        return token
+
+    def getHash(self, username):
+        milliseconds = str(time() * 1000)
         data = username + milliseconds
-        hash_object = hashlib.sha256(data.encode('utf-8'))
-        return hash_object.hexdigest()
-    
-    def getHash(self):
-        milliseconds = str(time()*random.randrange(10000))
-        data = milliseconds
-        hash_object = hashlib.sha256(data.encode('utf-8'))
-        return hash_object.hexdigest()
+        return hashlib.sha256(data.encode('utf-8')).hexdigest()
 
-dao=UserDAO()
-print(dao.getHash("User1"))
+    def getHash2(self):
+        milliseconds = str(time() * random.randrange(10000))
+        return hashlib.sha256(milliseconds.encode('utf-8')).hexdigest()
 
-u=dao.login("masdasfare", "pare")
-print(u)
 
-milliseconds = str(time()*1000)
-print("Time in milliseconds since epoch", milliseconds)
-data = "Hola Mundo" + milliseconds
-print(data)
+# =======================
+# RESPONSE
+# =======================
 
-hash_object = hashlib.sha256(data.encode('utf-8'))
-token = hash_object.hexdigest()
-print(token)
+@dataclass
+class ApiResponse:
+    msg: str
+    coderesponse: str
+    data: any
+
+
+# =======================
+# DAO INSTANCE
+# =======================
+
+userDao = UserDAO()
+
+
+# =======================
+# ROUTES
+# =======================
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+
+    identifier = data.get('username')  # username o email
+    password = data.get('password')
+
+    user = userDao.login(identifier, password)
+
+    if user:
+        response = ApiResponse(
+            msg="Authenticated",
+            coderesponse="1",
+            data=user
+        )
+    else:
+        response = ApiResponse(
+            msg="Not authenticated",
+            coderesponse="0",
+            data=""
+        )
+
+    return jsonify(asdict(response)), 200
+
+
+# =======================
+# MAIN
+# =======================
+
+if __name__ == '__main__':
+    app.run(debug=True)
